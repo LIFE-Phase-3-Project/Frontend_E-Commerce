62954b6bb6013b80c7bd751d2cf76fa86fc15ce3
rgashi <rulega2003@gmail.com>
2024-07-05 22:38:50 +0200

    on hover change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,7 +43,7 @@ export default function NavBar() {
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}
                     className={classNames(
-                      item.current ? 'bg-on-hover-green text-cream' : 'text-cream hover:bg-on-hover-green hover:text-cream',
+                      item.current ? 'font-bold text-cream' : 'relative text-cream hover:text-cream cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]',
                       'rounded-md px-3 py-2 text-sm font-medium',
                     )}
                   >
