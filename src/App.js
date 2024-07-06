06c0795871238bc4dcc9883a39a91d03c6b39969
rgashi <rulega2003@gmail.com>
2024-07-06 13:39:29 +0200

    Nav and Footer style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import React from 'react';
-import NavBar from './components/NavBar.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
 
-      <NavBar/>
-
-
-
-      
-     
+      <NavBar className="sticky top-0 z-50"/>  {/**sticky might not work bc page too short to test */}
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+      <footer className=" mx-auto bg-custom-green rounded-t-lg shadow m-4 dark:bg-gray-800">
+        <div className="p-3 mx-7 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-cream sm:text-center dark:text-gray-400 text-xs">
+            © 2024 <a href="" className="hover:underline">Ecommerce</a>. All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-cream dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+  
