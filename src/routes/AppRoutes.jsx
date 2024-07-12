bd7ad5589264def45ee1172bdaaf739516dba688
Bini <erblinshabani.life@gmail.com>
2024-07-12 18:53:34 +0200

    Added Scroll To Top

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes } from "react-router-dom"
 import { DefaultRoutes } from "./default-routes/default-routes"
 import NavBar from '../layouts/navbar/NavBar'
 import Footer from '../layouts/footer/Footer'
+import ScrollToTop from "./ScrollToTop"
 
 export const AppRoutes = () => {
     return (
         <Router>
+            <ScrollToTop />
             <NavBar />
                 <Routes>
                     {DefaultRoutes}
