69f6f69fa3f3f34fb88ce5224b748ec1f8c1793c
Bini <erblinshabani.life@gmail.com>
2024-08-05 01:44:44 +0200

    Added show orders

--- src/components/cart-modal/CheckOut.jsx
+++ src/components/cart-modal/CheckOut.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useGetOrderByUserIdQuery } from '../../redux/api/ordersApi';
+import { useCreatePaymentMutation } from '../../redux/api/paymentsApi';
 
 export const CheckOut = () => {
     const { data } = useGetOrderByUserIdQuery(localStorage.getItem('userId') || null);
+    const [createPayment] = useCreatePaymentMutation(); 
 
     const [shippingInfo, setShippingInfo] = useState({
         name: '',
@@ -18,16 +20,19 @@ export const CheckOut = () => {
     const [shippingPrice, setShippingPrice] = useState(0); 
     const [totalAmount, setTotalAmount] = useState(0);
     const [showDiscount, setShowDiscount] = useState(false); 
+    const [showOrders, setShowOrders] = useState(false); 
     const [showOverlay, setShowOverlay] = useState(false); 
+    const [showOrdersOverlay, setShowOrdersOverlay] = useState(false); 
     const [attempts, setAttempts] = useState(0);
     const [canTryAgain, setCanTryAgain] = useState(true);
+    const [orders, setOrders] = useState(data || []); 
 
     const discountCodeInLocalStorage = localStorage.getItem('discount-code') || '';
 
     const getOrderTotal = () => {
         let total = 0;
-        for (let i = 0; i < data?.length; i++) {
-            total += data[i].orderTotal;
+        for (let i = 0; i < orders.length; i++) {
+            total += orders[i].orderTotal;
         }
         setTotalAmount(total);
     };
@@ -44,8 +49,19 @@ export const CheckOut = () => {
         setPaymentMethod(e.target.value);
     };
 
-    const handlePurchase = () => {
-        // Purchase logic here
+    const handlePurchase = async () => {
+        if (paymentMethod === 'stripe') {
+            try {
+                const paymentData = {
+                    amount: calculateTotalAfterDiscount(),
+                    shippingInfo,
+                };
+                await createPayment(paymentData).unwrap();
+            } catch (error) {
+                console.error('Payment creation failed:', error);
+            }
+        } else if (paymentMethod === 'cash') {
+        }
     };
 
     const calculateTotalAfterDiscount = () => {
@@ -55,6 +71,11 @@ export const CheckOut = () => {
     const toggleDiscountCode = () => {
         setShowDiscount(!showDiscount);
         setShowOverlay(!showOverlay);
+    };
+
+    const toggleOrders = () => {
+        setShowOrders(!showOrders);
+        setShowOrdersOverlay(!showOrdersOverlay);
     };
 
     const handleDiscountCodeSubmit = () => {
@@ -73,14 +94,33 @@ export const CheckOut = () => {
         }
     };
 
+
+
+    const handleRemoveOrder = (orderName) => {
+        if (orderName) { 
+            const updatedOrders = orders.filter(order => order.name !== orderName);
+            setOrders(updatedOrders);
+            getOrderTotal(); 
+        } else {
+            console.error("Order ID is undefined or invalid");
+        }
+    };
+    
     useEffect(() => {
-        getOrderTotal();
+        setOrders(data || []);
     }, [data]);
+
+    useEffect(() => {
+        getOrderTotal(); 
+    }, [orders]);
 
     return (
         <div className="checkout max-w-2xl mx-auto p-4 bg-white shadow-lg rounded-lg dark:bg-custom-blue">
             {showOverlay && (
                 <div className="fixed inset-0 bg-black opacity-50 z-40" onClick={toggleDiscountCode}></div>
+            )}
+            {showOrdersOverlay && (
+                <div className="fixed inset-0 bg-black opacity-50 z-40" onClick={toggleOrders}></div>
             )}
 
             <div className="summary mb-6 dark:bg-custom-blue">
@@ -90,36 +130,54 @@ export const CheckOut = () => {
                         Discount Code
                     </button>
                 )}
-                {showDiscount && (
-                  <div className="discount-code mt-4 p-4 bg-gray-100 border rounded-md dark:bg-custom-blue z-50 relative">
-                    <label htmlFor='discount-code' className="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                        Discount code
-                    </label>
-                    <input 
-                        type="text" 
-                        id="discount-code" 
-                        value={discountCode}
-                        onChange={(e) => setDiscountCode(e.target.value)}
-                        placeholder='Enter discount code' 
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm dark:bg-gray-800 dark:border-gray-600 dark:text-gray-100"
-                    />
-                    
-                    {canTryAgain && attempts > 0 && (
-                        <p className="text-gray-600 dark:text-gray-300">Remaining Attempts: {3 - attempts}</p>
-                    )}
+                <button onClick={toggleOrders} className="bg-blue-500 ml-2 text-white py-2 px-4 rounded">
+                    Show orders
+                </button>
 
-                    <button
-                        onClick={handleDiscountCodeSubmit}
-                        disabled={!canTryAgain}
-                        className={`mt-2 w-full text-white py-2 rounded-md ${!canTryAgain ? 'bg-gray-400' : 'bg-green-600'}`}
-                    >
-                        Apply Discount
-                    </button>
-                    
-                    {!canTryAgain && (
-                        <p className="text-red-500 mt-2">You've exceeded the maximum attempts. Try again later.</p>
-                    )}
-                </div>
+                {showDiscount && (
+                    <div className="discount-code mt-4 p-4 bg-gray-100 border rounded-md dark:bg-custom-blue z-50 relative">
+                        <label htmlFor='discount-code' className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                            Discount code
+                        </label>
+                        <input 
+                            type="text" 
+                            id="discount-code" 
+                            value={discountCode}
+                            onChange={(e) => setDiscountCode(e.target.value)}
+                            placeholder='Enter discount code' 
+                            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm dark:bg-gray-800 dark:border-gray-600 dark:text-gray-100"
+                        />
+                        
+                        {canTryAgain && attempts > 0 && (
+                            <p className="text-gray-600 dark:text-gray-300">Remaining Attempts: {3 - attempts}</p>
+                        )}
+
+                        <button
+                            onClick={handleDiscountCodeSubmit}
+                            disabled={!canTryAgain}
+                            className={`mt-2 w-full text-white py-2 rounded-md ${!canTryAgain ? 'bg-gray-400' : 'bg-green-600'}`}
+                        >
+                            Apply Discount
+                        </button>
+                        
+                        {!canTryAgain && (
+                            <p className="text-red-500 mt-2">You've exceeded the maximum attempts. Try again later.</p>
+                        )}
+                    </div>
+                )}
+
+                {showOrders && (
+                    <div className="discount-code mt-4 p-4 bg-gray-100 border rounded-md dark:bg-custom-blue z-50 relative ">
+                        <h2 className='text-center text-xl'>Orders</h2>
+                        {
+                            orders?.map((order) => (
+                                <div key={order.id} className='flex items-center justify-between'>
+                                    <h2>{order.name} | {order.orderTotal}&#8364;</h2>
+                                    <button onClick={() => handleRemoveOrder(order.name)} className="text-red-500">Remove</button>
+                                </div>
+                            ))
+                        }
+                    </div>
                 )}
                 <p className="mb-2 dark:text-cream">Total Amount: <span>${totalAmount.toFixed(2)}</span></p>
                 <p className="mb-2 dark:text-cream">Discount: <span className="text-green-400">-${discountAmount.toFixed(2)}</span></p>
