6ad3e586266cafa66e1b89fb1d3cbc208a8d3827
Bini <erblinshabani.life@gmail.com>
2024-08-04 03:09:36 +0200

    Minor Tweak

--- src/components/products/product-section/products-cards/ProductCard.jsx
+++ src/components/products/product-section/products-cards/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { AddToWishListIcon } from "./AddToWishListIcon";
 import { ProductCardImage } from "./ProductCardImage";
 import { ProductCardContent } from "./ProductCardContent";
@@ -6,10 +6,7 @@ import { useSelector } from "react-redux";
 import { ProductsProductCardSkeleton } from "../../../../helpers/skeletons/ProductsProductCard";
 
 export const ProductCard = ({ isLoading, product }) => {
-    const location = useLocation();
-    const pathname = location.pathname;
-    const segmentList = pathname.split("/").filter(segment => segment !== "");
-    const lastSegment = segmentList[segmentList.length - 1];
+    const params = useParams();
 
     const isUserLoggedIn = useSelector(state => state.user.isLoggedIn)
     if(isLoading) return <ProductsProductCardSkeleton />
@@ -18,7 +15,12 @@ export const ProductCard = ({ isLoading, product }) => {
         <div className="product-card relative w-64 h-96 mx-auto mt-10 lg:mt-6 flex flex-col">
             {isUserLoggedIn && <AddToWishListIcon product={product}/>}
 
-            <Link to={`${lastSegment === "products" ? product?.categoryId + "/" + product.id : product.id }`} className="w-full h-full flex flex-col">
+            <Link to={`${params?.subCategory
+                    ? product.id 
+                    : params?.category
+                        ? product.subCategoryId + "/" + product.id
+                        : product.categoryId + "/" + product.subCategoryId + "/" + product.id
+                    }`} className="w-full h-full flex flex-col">
                 <ProductCardImage product={product}/>
                 <ProductCardContent product={product}/>
             </Link>
